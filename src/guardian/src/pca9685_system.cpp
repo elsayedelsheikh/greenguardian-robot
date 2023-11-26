9ecf2a19203c3f789ee3590a0741dbb8b57e79be
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2023-11-27 01:17:10 +0200

    ...

--- src/guardian/src/pca9685_system.cpp
+++ src/guardian/src/pca9685_system.cpp
@@ -125,7 +125,7 @@ hardware_interface::CallbackReturn Pca9685SystemHardware::on_activate(
 {
   hw_interfaces_[0].position = 0.0;
   hw_interfaces_[1].position = 3.2;
-  hw_interfaces_[2].position = 3.14;
+  hw_interfaces_[2].position = 3.2;
   hw_interfaces_[3].position = 0.0;
   hw_interfaces_[4].position = 0.0;
   
